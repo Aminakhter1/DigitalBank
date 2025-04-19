fcf6a07ac8001df6c343832b2bae7b63cdbbf7ee
AMIN AKHTER <120783238+Aminakhter1@users.noreply.github.com>
2025-04-19 13:43:50 +0530

    digitalBank webapp
    It is abanking application using mongoDb,express,node and react.

---
+++ Backend/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);// Allows querying with fields not defined in the schema
+
+const connectToMongoose = async () => {
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Connected to Mongoose Through ${db.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = {
+  connectToMongoose,
+};
